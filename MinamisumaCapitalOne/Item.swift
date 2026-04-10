94b879799367a4c4b52c0fcd46334de29ce50880
LUISJG57 <a00836928@tec.mx>
2026-04-10 10:55:14 -0600

    initial commit

---
+++ MinamisumaCapitalOne/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MinamisumaCapitalOne
+//
+//  Created by Luis Garcia on 4/10/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MinamisumaCapitalOneTests/MinamisumaCapitalOneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinamisumaCapitalOneTests.swift
+//  MinamisumaCapitalOneTests
+//
+//  Created by Luis Garcia on 4/10/26.
+//
+
+import Testing
+@testable import MinamisumaCapitalOne
+
+struct MinamisumaCapitalOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
